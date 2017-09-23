5e48d8d00f79140e208a3869efbf32f656bf186e
Ajay Singh Rawat <asrawat.contact@gmail.com>
2017-09-23 13:56:17 +0530

    Rest Api's

--- app/Http/Controllers/ManagePatientController.php
+++ app/Http/Controllers/ManagePatientController.php
@@ -16,7 +16,79 @@ class ManagePatientController extends Controller
 	}
 
 	public function patientData($crno, $device_id){
-		//dd('welcome', $crno, $device_id);
-		return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+		/*
+		Default Json View
+
+		{
+			"status": true,
+			"data": {
+				"patient_name": "ABC",
+				"department": "Medicine",
+				"floor": 2,
+				"room_number": 45,
+				"doctor_name": "Dr. Summit Verma",
+				"patient_phone": 9914466774,
+				"patient_email": "hjhjhj@gmail.com",
+				"crno": 545454545,
+				"waiting_hall": "XZ",
+				"patient_token": 5,
+				"token_status": 1,
+				"token_total": 10
+			}
+		}
+		*/
+		return response()->json(['status' => true, 'data' => []]);
+	}
+
+	public function isDoctorValid($doctor_phone){
+		/*
+		Default Json
+
+			{
+				"status": true,
+				"otp": 1234,
+				"data": {
+					"name": "Dr.Summet Verma",
+					"phone": 9914477885,
+					"department": "Medicine",
+					"floor": 2,
+					"room_no": 45,
+					"token_total": 50
+				}
+			}
+
+		*/
+		return response()->json(['status' => true, 'data' => []]);
+	}
+
+	public function tokenStatus($doctor_phone){
+		/*
+		Default Json
+
+			{
+			"status": true,
+			"otp": 1234,
+			"data": {
+				"patient": {
+					"name": "ABC",
+							"department": "Medicine",
+							"floor": 2,
+							"room_number": 45,
+							"doctor_name": "Dr. Summit Verma",
+							"phone": 9914466774,
+							"email": "hjhjhj@gmail.com",
+							"crno": 545454545,
+							"waiting_hall": "XZ",
+							"token": 5
+				},
+				"token":{
+				"status": 1,
+						"total": 10
+				}
+			}
+			}
+
+		*/
+		return response()->json(['status' => true, 'data' => []]);
 	}
 }
